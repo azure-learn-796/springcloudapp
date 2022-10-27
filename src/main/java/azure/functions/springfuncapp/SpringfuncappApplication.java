668bff9dbc91a0f6d3864ad653d91e8d21f41c95
Shota Nagumo <shota-nagumo@scsac.co.jp>
2022-10-27 16:37:10 +0900

    initial

---
+++ src/main/java/azure/functions/springfuncapp/SpringfuncappApplication.java
@@ -0,0 +1,13 @@
+package azure.functions.springfuncapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringfuncappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringfuncappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/azure/functions/springfuncapp/SpringfuncappApplicationTests.java
@@ -0,0 +1,13 @@
+package azure.functions.springfuncapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringfuncappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
